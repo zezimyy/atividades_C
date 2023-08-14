b365beb50672e2df22b9094b537e4cd30e67b0c3
André Luis <66038323+zezimyy@users.noreply.github.com>
2023-08-14 12:14:28 -0300

    Update Maior_Media_Veiculos.c

--- Maior_Media_Veiculos.c
+++ Maior_Media_Veiculos.c
@@ -1,30 +1,53 @@
 #include <stdio.h>
 
 int main(void) {
-  float salario;
-  float inss;
-  
-  printf("Digite seu salario: ");
-  scanf("%f", &salario);
 
-  if (salario < 1693.72){
-    printf("Voce tem um desconto de 8%% no inss\n");
-    inss = salario * 0.08;
-    printf("%.2f", inss);
-  } 
-  if (salario >= 1693.73 && salario <= 2822.9){
-    printf("Voce tem um disconto de 9%% no inss\n");
-    inss = salario * 0.09;
-    printf("%.2f", inss);
-  }
-  if (salario >= 2822.91 && salario <= 5646.8){
-    printf("Voce tem um desconto de 11%% no inss\n");
-    inss = salario * 0.11;
-    printf("%.2f", inss);
-  }
-  if (salario > 5646.81){
-    printf("Voce tem um desconto de 621,04 reais no inss\n");
-  }
+  int carros[3];
+  int ano = 2017;
   
+  printf("Qual a quantidade de carro no 1° ano? ");
+  scanf("%d", &carros[0]);
+  printf("Qual a quantidade de moto no 1° ano? ");
+  scanf("%d", &carros[1]);
+  printf("Qual a quantidade de caminhão no 1° ano? ");
+  scanf("%d", &carros[2]);
+
+  float media = (carros[0] + carros[2] + carros[1]) / 3;
+  int maior_media = media;
+
+  printf("A media de veiculos nos 1° anos foi de: %.2f", media);
+
+    
+  printf("\n\nQual a quantidade de carro no 2° ano? ");
+  scanf("%d", &carros[0]);
+  printf("Qual a quantidade de moto no 2° ano? ");
+  scanf("%d", &carros[1]);
+  printf("Qual a quantidade de caminhão no 2° ano? ");
+  scanf("%d", &carros[2]);
+
+  media = (carros[0] + carros[2] + carros[1]) / 3;
+  if(media > maior_media){
+      maior_media = media;
+      ano+= 1;
+  };
+
+  printf("A media de veiculos nos 2° anos foi de: %.2f", media);
+
+
+  printf("\n\nQual a quantidade de carro no 3° ano? ");
+  scanf("%d", &carros[0]);
+  printf("Qual a quantidade de moto no 3° ano? ");
+  scanf("%d", &carros[1]);
+  printf("Qual a quantidade de caminhão no 3° ano? ");
+  scanf("%d", &carros[2]);
+
+  media = (carros[0] + carros[2] + carros[1]) / 3;
+  if(media > maior_media){
+      maior_media = media;
+      ano+=1;
+  };
+  printf("\nA media de veiculos nos 3° anos foi de: %.2f", media);
+  printf("\n\nA maior media foi: %.2f", maior_media);
+  printf("\nO ano com maior media foi %d", ano);
   return 0;
 }
